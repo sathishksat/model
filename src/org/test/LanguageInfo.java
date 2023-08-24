619a0eed0ffc8e6a2f4121810cec439b2c2920ce
sathishksat <sathishksat@gmail.com>
2023-08-24 09:04:05 +0530

    language

---
+++ src/org/test/LanguageInfo.java
@@ -0,0 +1,29 @@
+package org.test;
+
+public class LanguageInfo {
+	private void tamilLanguage() {
+		System.out.println("Taamil language is : Old Language");
+		
+	}
+	private void englishLanguage() {
+		System.out.println("English language is : British Language");
+	}
+
+	private void hindiLanguage() {
+		System.out.println("Hindi language also called : Samskrit Language ");
+		
+	}
+		
+	public static void main(String[] args) {
+		
+	LanguageInfo ed = new LanguageInfo();
+
+	ed.tamilLanguage();
+	ed.englishLanguage();
+	ed.hindiLanguage();
+
+	
+	}
+	}
+
+
