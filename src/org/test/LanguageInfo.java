6dfbbdf7615fc892f16287d31b398dcba5e045dd
sathishksat <sathishksat@gmail.com>
2023-08-24 09:16:31 +0530

    new sathsih

---
+++ src/org/test/LanguageInfo.java
@@ -0,0 +1,33 @@
+package org.test;
+
+
+	public class LanguageInfo {
+	private void tamilLanguage() {
+		System.out.println("Taamil language is : Old Language");
+		
+	}
+	private void englishLanguage() {
+		System.out.println("English language is : British Language");
+	}
+
+	private void hindiLanguage() {
+		System.out.println("Hindi language also called : Samskrit Language ");
+		
+	private void teluguLanguage() {
+		System.out.println("Telugu Language is spoke in Andra");
+	}
+	}
+		
+	public static void main(String[] args) {
+		
+	LanguageInfo ed = new LanguageInfo();
+
+	ed.tamilLanguage();
+	ed.englishLanguage();
+	ed.hindiLanguage();
+	
+	
+	}
+	}
+
+
